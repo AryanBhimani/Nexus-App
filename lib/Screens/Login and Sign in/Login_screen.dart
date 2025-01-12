d33da274f658aa80953786905f084174527b4888
aryanbhimani <aryan.bhimani.93@gmail.com>
2025-01-12 17:43:10 +0530

    first commit

--- lib/Screens/Login and Sign in/Login_screen.dart
+++ lib/Screens/Login and Sign in/Login_screen.dart
@@ -1,9 +1,176 @@
-// ignore: file_names
+// // ignore: file_names
+// import 'package:animate_do/animate_do.dart';
+// import 'package:flutter/material.dart';
+
+// class LoginPage extends StatelessWidget {
+//   const LoginPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       resizeToAvoidBottomInset: false,
+//       backgroundColor: Colors.white,
+//       appBar: AppBar(
+//         elevation: 0,
+//         backgroundColor: Colors.white,
+//         leading: IconButton(
+//           onPressed: () {
+//             Navigator.pop(context);
+//           },
+//           icon: const Icon(Icons.arrow_back_ios, size: 20, color: Colors.black,),
+//         ),
+//       ),
+//       body: SizedBox(
+//         height: MediaQuery.of(context).size.height,
+//         width: double.infinity,
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//           children: <Widget>[
+//             Expanded(
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                 children: <Widget>[
+//                   Column(
+//                     children: <Widget>[
+//                       FadeInUp(duration: const Duration(milliseconds: 1000), child: const Text("Login", style: TextStyle(
+//                         fontSize: 30,
+//                         fontWeight: FontWeight.bold
+//                       ),)),
+//                       const SizedBox(height: 20,),
+//                       FadeInUp(duration: const Duration(milliseconds: 1200), child: Text("Login to your account", style: TextStyle(
+//                         fontSize: 15,
+//                         color: Colors.grey[700]
+//                       ),)),
+//                     ],
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.symmetric(horizontal: 40),
+//                     child: Column(
+//                       children: <Widget>[
+//                         FadeInUp(duration: const Duration(milliseconds: 1200), child: makeInput(label: "Email")),
+//                         FadeInUp(duration: const Duration(milliseconds: 1300), child: makeInput(label: "Password", obscureText: true)),
+//                       ],
+//                     ),
+//                   ),
+//                   FadeInUp(duration: const Duration(milliseconds: 1400), child: Padding(
+//                     padding: const EdgeInsets.symmetric(horizontal: 40),
+//                     child: Container(
+//                       padding: const EdgeInsets.only(top: 3, left: 3),
+//                       decoration: BoxDecoration(
+//                         borderRadius: BorderRadius.circular(50),
+//                         border: const Border(
+//                           bottom: BorderSide(color: Colors.black),
+//                           top: BorderSide(color: Colors.black),
+//                           left: BorderSide(color: Colors.black),
+//                           right: BorderSide(color: Colors.black),
+//                         )
+//                       ),
+//                       child: MaterialButton(
+//                         minWidth: double.infinity,
+//                         height: 60,
+//                         onPressed: () {},
+//                         color: Colors.greenAccent,
+//                         elevation: 0,
+//                         shape: RoundedRectangleBorder(
+//                           borderRadius: BorderRadius.circular(50)
+//                         ),
+//                         child: const Text("Login", style: TextStyle(
+//                           fontWeight: FontWeight.w600, 
+//                           fontSize: 18
+//                         ),),
+//                       ),
+//                     ),
+//                   )),
+//                   FadeInUp(duration: const Duration(milliseconds: 1500), child: const Row(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: <Widget>[
+//                       Text("Don't have an account?"),
+//                       Text("Sign up", style: TextStyle(
+//                         fontWeight: FontWeight.w600, fontSize: 18
+//                       ),),
+//                     ],
+//                   ))
+//                 ],
+//               ),
+//             ),
+//             FadeInUp(duration: const Duration(milliseconds: 1200), child: Container(
+//               height: MediaQuery.of(context).size.height / 3,
+//               decoration: const BoxDecoration(
+//                 image: DecorationImage(
+//                   image: AssetImage('assets/background.png'),
+//                   fit: BoxFit.cover
+//                 )
+//               ),
+//             ))
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget makeInput({label, obscureText = false}) {
+//     return Column(
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       children: <Widget>[
+//         Text(label, style: const TextStyle(
+//           fontSize: 15,
+//           fontWeight: FontWeight.w400,
+//           color: Colors.black87
+//         ),),
+//         const SizedBox(height: 5,),
+//         TextField(
+//           obscureText: obscureText,
+//           decoration: InputDecoration(
+//             contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+//             enabledBorder: OutlineInputBorder(
+//               borderSide: BorderSide(color: Colors.grey.shade400)
+//             ),
+//             border: OutlineInputBorder(
+//               borderSide: BorderSide(color: Colors.grey.shade400)
+//             ),
+//           ),
+//         ),
+//         const SizedBox(height: 30,),
+//       ],
+//     );
+//   }
+// }
+
+
+
+
 import 'package:animate_do/animate_do.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _login() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Login Successful!')),
+      );
+    } on FirebaseAuthException catch (e) {
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.message ?? 'Login Failed')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +184,7 @@ class LoginPage extends StatelessWidget {
           onPressed: () {
             Navigator.pop(context);
           },
-          icon: const Icon(Icons.arrow_back_ios, size: 20, color: Colors.black,),
+          icon: const Icon(Icons.arrow_back_ios, size: 20, color: Colors.black),
         ),
       ),
       body: SizedBox(
@@ -32,105 +199,139 @@ class LoginPage extends StatelessWidget {
                 children: <Widget>[
                   Column(
                     children: <Widget>[
-                      FadeInUp(duration: const Duration(milliseconds: 1000), child: const Text("Login", style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold
-                      ),)),
-                      const SizedBox(height: 20,),
-                      FadeInUp(duration: const Duration(milliseconds: 1200), child: Text("Login to your account", style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.grey[700]
-                      ),)),
+                      FadeInUp(
+                        duration: const Duration(milliseconds: 1000),
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      FadeInUp(
+                        duration: const Duration(milliseconds: 1200),
+                        child: Text(
+                          "Login to your account",
+                          style: TextStyle(
+                              fontSize: 15, color: Colors.grey[700]),
+                        ),
+                      ),
                     ],
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 40),
                     child: Column(
                       children: <Widget>[
-                        FadeInUp(duration: const Duration(milliseconds: 1200), child: makeInput(label: "Email")),
-                        FadeInUp(duration: const Duration(milliseconds: 1300), child: makeInput(label: "Password", obscureText: true)),
+                        FadeInUp(
+                          duration: const Duration(milliseconds: 1200),
+                          child: makeInput(
+                              label: "Email",
+                              controller: _emailController,
+                              obscureText: false),
+                        ),
+                        FadeInUp(
+                          duration: const Duration(milliseconds: 1300),
+                          child: makeInput(
+                              label: "Password",
+                              controller: _passwordController,
+                              obscureText: true),
+                        ),
                       ],
                     ),
                   ),
-                  FadeInUp(duration: const Duration(milliseconds: 1400), child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 40),
-                    child: Container(
-                      padding: const EdgeInsets.only(top: 3, left: 3),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        border: const Border(
-                          bottom: BorderSide(color: Colors.black),
-                          top: BorderSide(color: Colors.black),
-                          left: BorderSide(color: Colors.black),
-                          right: BorderSide(color: Colors.black),
-                        )
-                      ),
-                      child: MaterialButton(
-                        minWidth: double.infinity,
-                        height: 60,
-                        onPressed: () {},
-                        color: Colors.greenAccent,
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50)
+                  FadeInUp(
+                    duration: const Duration(milliseconds: 1400),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Container(
+                        padding: const EdgeInsets.only(top: 3, left: 3),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          border: const Border(
+                            bottom: BorderSide(color: Colors.black),
+                            top: BorderSide(color: Colors.black),
+                            left: BorderSide(color: Colors.black),
+                            right: BorderSide(color: Colors.black),
+                          ),
                         ),
-                        child: const Text("Login", style: TextStyle(
-                          fontWeight: FontWeight.w600, 
-                          fontSize: 18
-                        ),),
+                        child: MaterialButton(
+                          minWidth: double.infinity,
+                          height: 60,
+                          onPressed: _login,
+                          color: Colors.greenAccent,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600, fontSize: 18),
+                          ),
+                        ),
                       ),
                     ),
-                  )),
-                  FadeInUp(duration: const Duration(milliseconds: 1500), child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text("Don't have an account?"),
-                      Text("Sign up", style: TextStyle(
-                        fontWeight: FontWeight.w600, fontSize: 18
-                      ),),
-                    ],
-                  ))
+                  ),
+                  FadeInUp(
+                    duration: const Duration(milliseconds: 1500),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("Don't have an account?"),
+                        Text(
+                          "Sign up",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600, fontSize: 18),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
-            FadeInUp(duration: const Duration(milliseconds: 1200), child: Container(
-              height: MediaQuery.of(context).size.height / 3,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/background.png'),
-                  fit: BoxFit.cover
-                )
+            FadeInUp(
+              duration: const Duration(milliseconds: 1200),
+              child: Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/linkedin.png'),
+                      fit: BoxFit.cover),
+                ),
               ),
-            ))
+            )
           ],
         ),
       ),
     );
   }
 
-  Widget makeInput({label, obscureText = false}) {
+  Widget makeInput(
+      {required String label,
+      required TextEditingController controller,
+      bool obscureText = false}) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Text(label, style: const TextStyle(
-          fontSize: 15,
-          fontWeight: FontWeight.w400,
-          color: Colors.black87
-        ),),
-        const SizedBox(height: 5,),
+        Text(
+          label,
+          style: const TextStyle(
+              fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+        ),
+        const SizedBox(height: 5),
         TextField(
+          controller: controller,
           obscureText: obscureText,
           decoration: InputDecoration(
-            contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
             enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey.shade400)
-            ),
+                borderSide: BorderSide(color: Colors.grey.shade400)),
             border: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey.shade400)
-            ),
+                borderSide: BorderSide(color: Colors.grey.shade400)),
           ),
         ),
-        const SizedBox(height: 30,),
+        const SizedBox(height: 30),
       ],
     );
   }
