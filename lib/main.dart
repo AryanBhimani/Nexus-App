64e492f0833c3f5f709c79ece2da9df64158b428
aryanbhimani <aryan.bhimani.93@gmail.com>
2025-01-30 15:35:41 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,30 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:nexusapp/Screens/Splash/Splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_notification_channel/flutter_notification_channel.dart';
+import 'package:flutter_notification_channel/notification_importance.dart';
 
-void main() async {
+import 'firebase_options.dart';
+import 'screens/splash_screen.dart';
+
+//global object for accessing device screen size
+late Size mq;
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+
+  //enter full-screen
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
+  await _initializeFirebase();
+
+  //for setting orientation to portrait only
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((value) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -13,10 +32,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Nexus',
-      debugShowCheckedModeBanner: false,
-      home: OnboardingPage(),
-    ); 
+    return MaterialApp(
+        title: 'Nexus',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            useMaterial3: false,
+            appBarTheme: const AppBarTheme(
+              centerTitle: true,
+              elevation: 1,
+              iconTheme: IconThemeData(color: Colors.black),
+              titleTextStyle: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 19),
+              backgroundColor: Colors.white,
+            )),
+        home: const SplashScreen());
   }
+}
+
+Future<void> _initializeFirebase() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  var result = await FlutterNotificationChannel().registerNotificationChannel(
+      description: 'For Showing Message Notification',
+      id: 'chats',
+      importance: NotificationImportance.IMPORTANCE_HIGH,
+      name: 'Chats');
+
+  log('\nNotification Channel Result: $result');
 }
